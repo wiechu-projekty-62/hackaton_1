b58f949eb053a237e48ecf77d1e7d2e36f734b12
wiechu-projekty-62 <wdudziak2210@gmail.com>
2022-05-28 22:04:06 +0200

    turtle

--- wisielec.py
+++ wisielec.py
@@ -1,9 +1,11 @@
 from random import choice
+from turtle import *
 def wisielec():
     slownik = wczytaj("miasta.txt")
     password = choice(slownik)
     guessed = "*" * len(password)
     trials = 10
+    rysunek(trials)
 
     while guessed != password and trials > 0:
         print("Zgadnij hasło", guessed)
@@ -21,6 +23,7 @@ def wisielec():
                     print("nieprawidłowe hasło :-(")
                     trials -= 1
                     print("Pozostało prób: ", trials)
+                    rysunek(trials)
             else:
                 allguessed = ""
                 break
@@ -34,10 +37,13 @@ def wisielec():
                         guessed = guessed[:i] + response + guessed[i + 1:]
             else:
                 trials -= 1
+                rysunek(trials)
+
     if guessed == password or allguessed == password:
         print("Odgadłeś - hasło to", password)
     else:
         print("nieprawidłowe hasło :-(")
+
 def wczytaj(nazwa):
     ident = open(nazwa)
     lista = []
@@ -45,6 +51,59 @@ def wczytaj(nazwa):
         lista.append(x.strip())
     ident.close()
     return lista
+
+def rysunek(nr):
+
+    #czyszczenie
+    seth(0); setpos(0, 0); clear()
+    if nr == 10: return
+
+    #podstawa
+    bk(80); fd(40); lt(90)
+    if nr == 9: return
+    #pion
+    fd(200); rt(90)
+    if nr == 8: return
+
+    #poziom
+    fd(100); rt(90)
+    if nr == 7: return
+
+    #haczyk
+    fd(30)
+    if nr == 6: return
+
+    #głowa
+    lt(90)
+    for i in range(36):
+        fd(3); rt(10)
+    rt(90)
+    if nr == 5: return
+
+    #tułów
+    pu(); fd(36); pd()
+    fd(60)
+    if nr == 4: return
+
+    #prawa ręka
+    bk(50); rt(45); fd(30); bk(30)
+    if nr ==3: return
+
+    #lewa ręka
+    lt(90); fd(30); bk(30); rt(45)
+    if nr == 2: return
+
+    #prawa noga
+    fd(50); rt(30); fd(50); bk(50)
+    if nr == 1: return
+
+    #lewa noga
+    lt(60); fd(50); bk(50); rt(45)
+
+    pu()
+    ht()
+    x = input("Zakończ wyswietlanie grafiki [enter]")
+
 wisielec()
 
 
